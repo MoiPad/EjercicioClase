2f73e3395cf7141bc0a07ee0e8c7d27e053b74b7
MoiPad92 <moipad85@gmail.com>
2024-01-22 23:14:13 -0600

    update

---
+++ EjercicioClase/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// objetos
+
+
+using EjercicioClase;
+
+
+
+        SuperHeroe Batman = new SuperHeroe();
+        Batman.Nombre = "Batman";
+        Batman.IdentidadSecreta = "Bruno Diaz";
+        Batman.Ciudad = "Gotica";
+        Batman.PuedeVolar = false;
+        Batman.Poder.Nombre = "No tiene";
+        Batman.Poder.Descripcion = "Habilidades como detective";
+        Batman.Poder.Nivel = 5;
+
+        Batman.Imprimir();
+        
---
+++ EjercicioClase/SuperHeroe.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioClase
+{
+    internal class SuperHeroe
+    {
+
+        public string Nombre { get; set; }
+        public string IdentidadSecreta { get; set; }
+        public string Ciudad { get; set; }
+        public bool PuedeVolar { get; set; }
+        public SuperPoder Poder { get; set; }
+        public SuperHeroe(string nombre, string identidadsecreta, string ciudad, bool puedevolar, SuperPoder poder)
+        {
+            Nombre = nombre;
+            IdentidadSecreta = identidadsecreta;
+            Ciudad = ciudad;
+            PuedeVolar = puedevolar;
+        }
+
+        public SuperHeroe()
+        {
+           Poder = new SuperPoder();
+        }
+     
+
+        public void Imprimir() 
+        {
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Identidad Secreta: {IdentidadSecreta}");
+            Console.WriteLine($"Ciudad: {Ciudad}");
+            Console.WriteLine($"Puede volar: {PuedeVolar}");
+            Console.WriteLine($"Super Poder: {Poder.Nombre}");
+            Console.WriteLine($"Descripcion: {Poder.Descripcion}");
+            Console.WriteLine($"Nivel: {Poder.Nivel}");
+        }
+    }
+}
